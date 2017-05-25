06e9c9c7e02f54374c7219f7612d68225a1875be
伍鹏宇 <pengyu.wu@happylifeplat.com>
2017-05-25 12:00:20 +0800

    init

---
+++ src/main/java/com/wpy/service/DubboA.java
@@ -0,0 +1,18 @@
+package com.wpy.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>Description:</p>
+ * <pre></pre>
+ * <p>Company: 深圳市旺生活互联网科技有限公司</p>
+ *
+ * @author wupengyu
+ * @date 2017/5/25 11:46
+ * @copyright 2015-2016 happylifeplat.com All Rights Reserved
+ */
+@Service
+public interface DubboA {
+
+    public void test(String flag);
+}
